62340c49994c9b4e27b107e1ddada20cc14c3eba
Mary Vo <maryvo.uk@gmail.com>
2023-02-05 01:10:30 -0500

    add section 4 notes

--- demos/m4/demo.py
+++ demos/m4/demo.py
@@ -1,66 +1,67 @@
 # -*- coding: utf-8 -*-
-
 import pandas as pd
-import numpy as np
 import os
 
+# Let's load the data for the first time
 df = pd.read_pickle(os.path.join('..', 'data_frame.pickle'))
 
-# ITERATION
-small_df = df.iloc[49980:50019, :].copy()
-grouped = small_df.groupby('artist')
-type(grouped)
+# Demo 1
+# Question: How many distinct artists are there in the dataset?
+# These two do the same thing, returns just artists using df.artist (not advised?) and df['artist']
+# print(df.artist)
+artists = df['artist']
+print(artists)
+print(f"Return unique artists: {pd.unique(artists)}")
+print(f"Return the count of distinct artists: {len(pd.unique(artists))}")
 
-for name, group_df in grouped:
-    print(name)
-    print(group_df)
-    break
+# Demo 2
+# Question: How many artworks by Francis Bacon are there?
+s = df['artist'] == 'Bacon, Francis'
+print(s)
+# Use value_count() to get a count of the records broken out by the boolean values
+print(s.value_counts())
+ 
+# # Another way to do the same as above
+artist_counts = df['artist'].value_counts()
+print(artist_counts['Bacon, Francis'])
 
-# Aggregate
-# Mins
-for name, group_df in small_df.groupby('artist'): 
-    min_year = group_df['acquisitionYear'].min()
-    print("{}: {}".format(name, min_year))
 
-# Transform
-# Equivalent of editing by hand:
-# Make a case when there is no data to infer
-# small_df.loc[[11838, 16441], 'medium'] = np.nan
-def fill_values(series):
-    values_counted = series.value_counts()
-    if values_counted.empty:
-        return series
-    most_frequent = values_counted.index[0]
-    new_medium = series.fillna(most_frequent)
-    return new_medium
+# Demo 3
+# Question: What is the artwork with the biggest dimensions? Dimension here is meant to be area (width x height)
+# loc by labels and iloc by position
+print(df.loc[1035, 'artist'])
+print(df.iloc[0, 0])
+print(df.iloc[0, :])
+print(df.iloc[0:2, 0:2])
 
-def transform_df(source_df):
-    group_dfs = []  
-    for name, group_df in source_df.groupby('artist'):
-        filled_df = group_df.copy()
-        filled_df.loc[:, 'medium'] = fill_values(group_df['medium'])
-        group_dfs.append(filled_df)
-    
-    new_df = pd.concat(group_dfs)
-    return new_df
+# Try multiplication
+# print(df['height'] * df['width'])  #Errors because there are non-numeric values 
+# Do some checking to see what the values look like, you will see there are some strings in the width column
+print(df['width'].sort_values().head())
+print(df['width'].sort_values().tail())
 
-# Now check the result
-filled_df = transform_df(small_df)
+# Try to convert, using to_numeric method
+# print(pd.to_numeric(df['width'])) #Error because pandas cannot change a string to a number
 
-# BUILT-INS
-# Transform
-grouped_mediums = small_df.groupby('artist')['medium']
-small_df.loc[:, 'medium'] = grouped_mediums.transform(fill_values)
+# So we can Force NaNs 
+# This basically says on error, coerce the value to NaN
+print(pd.to_numeric(df['width'], errors='coerce'))
+df.loc[:, 'width'] = pd.to_numeric(df['width'], errors='coerce')
 
-# Min
-df.groupby('artist').agg(np.min)
-df.groupby('artist').min()
+pd.to_numeric(df['height'], errors='coerce')
+df.loc[:, 'height'] = pd.to_numeric(df['height'],
+                                    errors='coerce')
 
-# Filter
-grouped_titles = df.groupby('title')
-title_counts = grouped_titles.size().sort_values(ascending=False)
+print(f"Area: {df['height'] * df['width']}")
+print(f"Units: {df['units'].value_counts()}")
 
-condition = lambda x: len(x.index) > 1
-dup_titles_df = grouped_titles.filter(condition)
-dup_titles_df.sort_values('title', inplace=True)
-    
+# Create and Assign new column for area in dataframe
+area = df['height'] * df['width']
+df = df.assign(area=area)
+print(df.head())
+
+# Now that we have the area, we need to find the artwork with the greatest area/dimension
+print(df['area'].max())
+# We can determine the index/row of the artwork with the largest dimension
+print(f"Print id or index of the artwork with the largest dimension: {df['area'].idxmax()}")
+print(f"Print information about the the artwork using the index: {df.loc[df['area'].idxmax(), :]}")
